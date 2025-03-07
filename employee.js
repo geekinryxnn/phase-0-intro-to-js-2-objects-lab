24fa56c5a4cbe82364b08e8e1813c5065a65ce3f
geekinryxnn <ryankiratu67@gmail.com>
2025-03-07 20:16:16 +0300

    new changes

---
+++ employee.js
@@ -0,0 +1,26 @@
+const employee = {
+    // ...existing code...
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
